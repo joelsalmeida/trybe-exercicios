87a98c7d29e77847276d182e66e996e606e913fe
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-08-27 15:28:17 -0300

    Inicia exercicio bonus em 01-JS- Primeiros passos.

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: JavaScript - Primeiros passos/exercices.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: JavaScript - Primeiros passos/exercices.js
@@ -266,3 +266,59 @@ function profitCalculation(cost, salesValue, tax) {
     }
 }
 profitCalculation(100, 90, 20);
+
+// Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR.
+// Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+
+// A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10.
+// Para as faixas de impostos, use as seguintes referências:
+
+// INSS (Instituto Nacional do Seguro Social)
+// Salário bruto até R$ 1.556,94: alíquota de 8%
+// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+
+// IR (Imposto de Renda)
+// Até R$ 1.903,98: isento de imposto de renda
+// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+
+function getNetSalary(grossSalary) {
+    let socialSecurityTax;
+    let incomeTax;
+    
+    function percent(value, perCentToGet) {
+        let result = (value / 100) * perCentToGet;
+        return result;
+    }
+
+    function getSocialSecurityTax(gs1) {
+        
+        let taxA = percent(gs1, 8);
+        let taxB = percent(gs1, 9);
+        let taxC = percent(gs1, 11);
+        let taxValueA = 570.88;
+
+        if (gs1 <= 1556.94) {
+            socialSecurityTax = taxA.toFixed(2);
+        } else if (gs1 >= 1556.95 && gs1 <= 2594.92) {
+            socialSecurityTax = taxB.toFixed(2);
+        } else if (gs1 >= 2594.93 && gs1 <= 5198.82) {
+            socialSecurityTax = taxC.toFixed(2);
+        } else {
+            socialSecurityTax = taxValueA;
+        }
+        console.log("social security tax: " + socialSecurityTax);
+    }
+    getSocialSecurityTax(grossSalary);
+
+    function getIncomeTax(gs2) {
+        
+    }
+
+}
+
+getNetSalary(1556.94);
