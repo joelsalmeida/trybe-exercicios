415ae6b80dc7903ca33f11abe2e7bc5e38975cd8
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-09-06 10:51:41 -0300

    Finaliza exercicios e refatora algumas funçoes.

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: JavaScript - Primeiros passos/exercices.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: JavaScript - Primeiros passos/exercices.js
@@ -8,164 +8,162 @@
 // Módulo (a % b)
 
 function sum(n1, n2) {
-    let result = n1 + n2;
-    console.log("sum: " + result);
+  let result = n1 + n2;
+  console.log("sum: " + result);
 }
 sum(5, 5);
 
 function subtraction(n1, n2) {
-    let result = n1 - n2;
-    console.log("subtraction: " + result);
+  let result = n1 - n2;
+  console.log("subtraction: " + result);
 }
 subtraction(10, 3);
 
 function multiplication(n1, n2) {
-    let result = n1 * n2;
-    console.log("multiplication: " + result);
+  let result = n1 * n2;
+  console.log("multiplication: " + result);
 }
 multiplication(10, 3);
 
 function division(n1, n2) {
-    let result = n1 / n2;
-    console.log("division: " + result);
+  let result = n1 / n2;
+  console.log("division: " + result);
 }
 division(10, 3);
 
 function remainder(n1, n2) {
-    let result = n1 % n2;
-    console.log("remainder: " + result);
+  let result = n1 % n2;
+  console.log("remainder: " + result);
 }
 remainder(7, 3);
 
-// Faça um programa que retorne o maior de dois números. 
+// Faça um programa que retorne o maior de dois números.
 // Defina no começo do programa duas constantes com os valores que serão comparados.
-
 function whoIsBigger(n1, n2) {
-    let result;
+  let result;
 
-    if (n1 > n2) {
-        result = n1;
-    } else {
-        result = n2;
-    }
-    console.log(result + " is bigger.")
+  if (n1 > n2) {
+    result = n1;
+  } else {
+    result = n2;
+  }
+  console.log(result + " is bigger.");
 }
 whoIsBigger(5, 3);
 
 // Faça um programa que retorne o maior de três números.
 // Defina no começo do programa três constantes com os valores que serão comparados.
-
 function whoIsBigger3(n1, n2, n3) {
-    let result;
+  let result;
 
-    if (n1 > n2 && n1 > n3) {
-        result = n1;
-    } else if (n2 > n1 && n2 > n3) {
-        result = n2;
-    } else result = n3;
-    console.log(result + " is bigger3.")
+  if (n1 > n2 && n1 > n3) {
+    result = n1;
+  } else if (n2 > n1 && n2 > n3) {
+    result = n2;
+  } else result = n3;
+  console.log(result + " is bigger3.");
 }
 whoIsBigger3(5, 4, 3);
 
 // Faça um programa que, dado um valor definido numa constante,
 // retorne "positive" se esse valor for positivo,
 // "negative" se for negativo e "zero" caso contrário.
-
 function positiveOrNegative(number) {
-    let result;
-    if (number < 0) {
-        result = "negative.";
-    } else if (number === 0) {
-        result = "zero.";
-    } else {
-        result = "positive."
-    }
-    console.log(result);
+  let result;
+  if (number < 0) {
+    result = "negative.";
+  } else if (number === 0) {
+    result = "zero.";
+  } else {
+    result = "positive.";
+  }
+  console.log(result);
 }
 positiveOrNegative(-1);
 
 // Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo.
 // Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário.
 // Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
-
 function aTriangle(a1, a2, a3) {
+  let angles = [a1, a2, a3];
+  let totalAngle = a1 + a2 + a3;
+  let validAngle;
+  let result;
 
-    let angles = [a1, a2, a3];
-    let totalAngle = a1 + a2 + a3;
-    let validAngle;
-    let result;
+  for (let index = 0; index < angles.length; index++) {
+    let thisAngle = angles[index];
 
-    for (let index = 0; index < angles.length; index++) {
-        let thisAngle = angles[index];
-
-        if (thisAngle < 0) {
-            validAngle = "invalid";
-        } else {
-            validAngle = "valid"
-        }
-        console.log(thisAngle + " is " + validAngle + ".")
+    if (thisAngle < 0) {
+      validAngle = "invalid";
+    } else {
+      validAngle = "valid";
     }
+    console.log(thisAngle + " is " + validAngle + ".");
+  }
 
-    switch (totalAngle) {
-        case 180:
-            result = "it's a triangle."
-            break;
+  switch (totalAngle) {
+    case 180:
+      result = "it's a triangle.";
+      break;
 
-        default:
-            result = "it's not a triangle."
-            break;
-    }
-    console.log(result);
+    default:
+      result = "it's not a triangle.";
+      break;
+  }
+  console.log(result);
 }
 aTriangle(-30, 140, 10);
 
 // Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
-
 function chessMovements(piece) {
-    let king = "The King may move one square in any direction, so long as no piece is blocking his path.";
+  let king =
+    "The King may move one square in any direction, so long as no piece is blocking his path.";
 
-    let queen = "The Queen may move any number of squares straight or diagonally in any direction.";
+  let queen =
+    "The Queen may move any number of squares straight or diagonally in any direction.";
 
-    let rook = "The Rook may move in a straight line, any number of squares horizontally or vertically.";
+  let rook =
+    "The Rook may move in a straight line, any number of squares horizontally or vertically.";
 
-    let bishop = "The Bishop may move any number of squares diagonally.";
+  let bishop = "The Bishop may move any number of squares diagonally.";
 
-    let knight = "The only piece that can jump over a piece - be it your own, or the opponent’s - is the: Knight.";
+  let knight =
+    "The only piece that can jump over a piece - be it your own, or the opponent’s - is the: Knight.";
 
-    let pawn = "The Pawn on its first move may move either one or two squares straight forward.";
+  let pawn =
+    "The Pawn on its first move may move either one or two squares straight forward.";
 
-    let pieceStandardized = piece.toLowerCase();
+  let pieceStandardized = piece.toLowerCase();
 
-    switch (pieceStandardized) {
-        case "king":
-            console.log(king)
-            break;
+  switch (pieceStandardized) {
+    case "king":
+      console.log(king);
+      break;
 
-        case "queen":
-            console.log(queen)
-            break;
+    case "queen":
+      console.log(queen);
+      break;
 
-        case "rook":
-            console.log(rook)
-            break;
+    case "rook":
+      console.log(rook);
+      break;
 
-        case "bishop":
-            console.log(bishop)
-            break;
+    case "bishop":
+      console.log(bishop);
+      break;
 
-        case "knight":
-            console.log(knight)
-            break;
+    case "knight":
+      console.log(knight);
+      break;
 
-        case "pawn":
-            console.log(pawn)
-            break;
-        default:
-            console.log("Ops. Does this piece exist?")
-            break;
-    }
+    case "pawn":
+      console.log(pawn);
+      break;
+    default:
+      console.log("Ops. Does this piece exist?");
+      break;
+  }
 }
-
 chessMovements("PAWN");
 
 // Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100)em conceitos de A a F.
@@ -177,41 +175,39 @@ chessMovements("PAWN");
 // Porcentagem >= 50 -> E
 // Porcentagem < 50 -> F
 // O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
-
 function yourGrade(g) {
-    let gradeFeedback;
+  let gradeFeedback;
 
-    if (g < 0 || g > 100) {
-        gradeFeedback = "Ops, something wrong.";
-    } else if (g >= 90) {
-        gradeFeedback = "A. You were amazing!";
-    } else if (g >= 80) {
-        gradeFeedback = "B. You did a great job!";
-    } else if (g >= 70) {
-        gradeFeedback = "C. Good work!";
-    } else if (g >= 60) {
-        gradeFeedback = "D. Nemo said - keep swimming...";
-    } else if (g >= 50) {
-        gradeFeedback = "E. You can improve.";
-    } else if (g < 50) {
-        gradeFeedback = "F. :(";
-    }
-    console.log(gradeFeedback);
+  if (g < 0 || g > 100) {
+    gradeFeedback = "Ops, something wrong.";
+  } else if (g >= 90) {
+    gradeFeedback = "A. You were amazing!";
+  } else if (g >= 80) {
+    gradeFeedback = "B. You did a great job!";
+  } else if (g >= 70) {
+    gradeFeedback = "C. Good work!";
+  } else if (g >= 60) {
+    gradeFeedback = "D. Nemo said - keep swimming...";
+  } else if (g >= 50) {
+    gradeFeedback = "E. You can improve.";
+  } else if (g < 50) {
+    gradeFeedback = "F. :(";
+  }
+  console.log(gradeFeedback);
 }
 yourGrade(-1);
 
 // Escreva um programa que defina três números em constantes
-// e retorne true se pelo menos uma das três for par. 
+// e retorne true se pelo menos uma das três for par.
 // Caso contrário, ele retorna false .
-
 function isEven(n1, n2, n3) {
-    let result;
-    if (n1 % 2 === 0 || n2 % 2 === 0 || n3 % 2 === 0) {
-        result = true;
-    } else {
-        result = false;
-    }
-    console.log("Is there any even number? " + result)
+  let result;
+  if (n1 % 2 === 0 || n2 % 2 === 0 || n3 % 2 === 0) {
+    result = true;
+  } else {
+    result = false;
+  }
+  console.log("Is there any even number? " + result);
 }
 isEven(3, 2, 7);
 
@@ -221,51 +217,49 @@ isEven(3, 2, 7);
 // Atente que, sobre o custo do produto, incide um imposto de 20%.
 // Seu programa também deve emitir uma mensagem de erro e encerrar
 // caso algum dos seus valores de entrada seja menor que zero.
+function checkValues(cost, salesValue, tax) {
+  let allValues = [cost, salesValue, tax];
+
+  allValues.forEach((element) => {
+    if (element < 0) {
+      console.log(element + " is a invalid value.");
+    } else {
+      console.log(element + " is a valid value.");
+    }
+  });
+}
+
+function profitResult(costPr, salesValuePr, taxPr) {
+  let taxValue = (costPr / 100) * taxPr;
+  console.log("The tax value is: " + taxValue);
+
+  let totalCost = costPr + taxValue;
+  console.log("The total cost is: " + totalCost);
+
+  let profit = salesValuePr - totalCost;
+
+  if (profit < 0) {
+    console.log("Looks like we have a loss of " + profit);
+  } else if (profit === 0) {
+    console.log("No profit. This seems like a waste of time and energy.");
+  } else {
+    console.log(
+      "Considering the tax of " + taxPr + " your profit is " + profit + "."
+    );
+    let profitPer1k = profit * 1000;
+    console.log("By selling 1000 pieces, the profit will be " + profitPer1k);
+  }
+}
 
 function profitCalculation(cost, salesValue, tax) {
-
-    function checkValues(v1, v2, v3) {
-
-        let allValues = [v1, v2, v3];
-
-        allValues.forEach(element => {
-            if (element < 0) {
-                console.log(element + " is a invalid value.")
-            } else {
-                console.log(element + " is a valid value.")
-            }
-        });
-    }
-
-    function profitResult(costPr, salesValuePr, taxPr) {
-
-        let taxValue = (costPr / 100) * taxPr;
-        console.log("The tax value is: " + taxValue);
-    
-        let totalCost = costPr + taxValue;
-        console.log("The total cost is: " + totalCost);
-    
-        let profit = salesValuePr - totalCost;
-        
-        if (profit < 0) {
-            console.log("Looks like we have a loss of " + profit);
-        } else if (profit === 0) {
-            console.log("No profit. This seems like a waste of time and energy.")
-        } else {
-            console.log("Considering the tax of " + taxPr + " your profit is " + profit + ".")
-            let profitPer1k = profit * 1000;
-            console.log("By selling 1000 pieces, the profit will be " + profitPer1k)
-        }
-    }
-
-    if (cost < 0 || salesValue < 0 || tax < 0) {
-        console.log("Ops. Something wrong. :(")
-        checkValues(cost, salesValue, tax);
-    } else {
-        profitResult(cost, salesValue, tax)
-    }
+  if (cost < 0 || salesValue < 0 || tax < 0) {
+    console.log("Ops. Something wrong. :(");
+    checkValues(cost, salesValue, tax);
+  } else {
+    profitResult(cost, salesValue, tax);
+  }
 }
-profitCalculation(100, 90, 20);
+profitCalculation(-100, 185, 20);
 
 // Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR.
 // Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
@@ -286,39 +280,61 @@ profitCalculation(100, 90, 20);
 // De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
 // Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
 
-function getNetSalary(grossSalary) {
-    let socialSecurityTax;
-    let incomeTax;
-    
-    function percent(value, perCentToGet) {
-        let result = (value / 100) * perCentToGet;
-        return result;
-    }
-
-    function getSocialSecurityTax(gs1) {
-        
-        let taxA = percent(gs1, 8);
-        let taxB = percent(gs1, 9);
-        let taxC = percent(gs1, 11);
-        let taxValueA = 570.88;
-
-        if (gs1 <= 1556.94) {
-            socialSecurityTax = taxA.toFixed(2);
-        } else if (gs1 >= 1556.95 && gs1 <= 2594.92) {
-            socialSecurityTax = taxB.toFixed(2);
-        } else if (gs1 >= 2594.93 && gs1 <= 5198.82) {
-            socialSecurityTax = taxC.toFixed(2);
-        } else {
-            socialSecurityTax = taxValueA;
-        }
-        console.log("social security tax: " + socialSecurityTax);
-    }
-    getSocialSecurityTax(grossSalary);
-
-    function getIncomeTax(gs2) {
-        
-    }
-
+function percent(value, perCentToGet) {
+  let result = (value / 100) * perCentToGet;
+  return result;
 }
 
-getNetSalary(1556.94);
+function getSocialSecurityTax(gs) {
+  let taxA = percent(gs, 8);
+  let taxB = percent(gs, 9);
+  let taxC = percent(gs, 11);
+  let taxD = 570.88;
+
+  if (gs <= 1556.94) {
+    socialSecurityTax = taxA.toFixed(2);
+  } else if (gs >= 1556.95 && gs <= 2594.92) {
+    socialSecurityTax = taxB.toFixed(2);
+  } else if (gs >= 2594.93 && gs <= 5198.82) {
+    socialSecurityTax = taxC.toFixed(2);
+  } else {
+    socialSecurityTax = taxD;
+  }
+  return socialSecurityTax;
+}
+
+function getIncomeTax(gs) {
+  let taxA = 0;
+  let taxB = percent(baseSalary, 7.5) - 142.8;
+  let taxC = percent(baseSalary, 15) - 354.8;
+  let taxD = percent(baseSalary, 22.5) - 636.13;
+  let taxE = percent(baseSalary, 27.5) - 869.36;
+
+  if (baseSalary <= 1903.98) {
+    incomeTax = taxA;
+  } else if (baseSalary >= 1903.99 && baseSalary <= 2826.65) {
+    incomeTax = taxB;
+  } else if (baseSalary >= 2826.66 && baseSalary <= 3751.05) {
+    incomeTax = taxC;
+  } else if (baseSalary >= 3751.06 && baseSalary <= 4664.68) {
+    incomeTax = taxD;
+  } else if (baseSalary > 4664.68) {
+    incomeTax = taxE;
+  }
+  return incomeTax;
+}
+
+let baseSalary;
+function getNetSalary(grossSalary) {
+  let socialSecurityTax;
+  let incomeTax;
+  let netSalary;
+
+  socialSecurityTax = getSocialSecurityTax(grossSalary);
+  baseSalary = grossSalary - socialSecurityTax;
+  incomeTax = getIncomeTax(grossSalary);
+  netSalary = baseSalary - incomeTax;
+
+  return netSalary;
+}
+getNetSalary(3000.0);
