85a91ff4bb6a0db45427e9881b890599a65902a7
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-09-23 18:53:21 -0300

    ES6: let,const, arrow... - Finaliza primeira parte dos exercicios.

---
+++ Fundamentos do Desenvolvimento Web/Bloco 07: Introdução à JavaScript ES6 e Testes Unitários/01: JavaScript ES6 - let, const, arrow functions e template literals/exercise.js
@@ -0,0 +1,39 @@
+// Faça as modificações necessárias na função para que o seu comportamento
+// respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function.
+// Modifique as concatenações para template literals.
+
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+// testingScope(true);
+
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+testingScope(false);
+
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>);
+// retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort().
+// Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sorted = oddsAndEvens.sort((a, b) => a - b);
+
+console.log(`Os números ${sorted} se encontram ordenados de forma crescente!`);
