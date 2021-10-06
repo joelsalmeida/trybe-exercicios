395ef1b268fdea51cf6eec5ce75de8065a2c4c9a
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-10-06 10:43:52 -0300

    Finaliza exercicio 06.

--- Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/02: JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/exercise.js
+++ Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/02: JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/exercise.js
@@ -177,3 +177,16 @@ function everyoneWasBornOnSecXX() {
 console.log(everyoneWasBornOnSecXX());
 
 assert.strictEqual(everyoneWasBornOnSecXX(), expectedResultSecXX);
+
+// 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+
+const expectedResultThe80s = true;
+
+function someBookWasReleaseOnThe80s() {
+  return books.some(function(book) {
+    return book.releaseYear >= 1980 && book.releaseYear <= 1989;
+  })
+}
+console.log(someBookWasReleaseOnThe80s());
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResultThe80s);
