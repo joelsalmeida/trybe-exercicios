04a35cc691ddcaecd61a1eedb1157754b702fbeb
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-10-06 09:23:10 -0300

    Finaliza exercicio 03.

--- Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/02: JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/exercise.js
+++ Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/02: JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/exercise.js
@@ -90,3 +90,23 @@ function smallerName() {
 console.log(smallerName());
 
 assert.strictEqual(smallerName(), 'Duna');
+
+// 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return books.find((book) => book.name.length === 26)
+}
+console.log(getNamedBook());
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
