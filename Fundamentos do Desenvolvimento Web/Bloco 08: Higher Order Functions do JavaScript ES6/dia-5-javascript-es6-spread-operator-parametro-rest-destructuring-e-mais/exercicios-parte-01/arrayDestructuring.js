caa68ebf70d1c33e00ebcf304a4422eb8c6ee5a1
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-10-08 18:30:15 -0300

    Cria funçao defaultDestructuring.

--- Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-parte-01/arrayDestructuring.js
+++ Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-parte-01/arrayDestructuring.js
@@ -34,6 +34,6 @@ console.log(numerosPares); // [6, 8, 10, 12];
 
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
 
-[,,, ...numerosPares] = numerosPares;
+[, , , ...numerosPares] = numerosPares;
 
 console.log(numerosPares);
