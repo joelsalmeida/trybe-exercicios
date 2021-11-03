61a0d2de9ef0c804b0f2d6c50aacbdabada59fd2
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-03 15:47:07 -0300

    Exercicio de fixaçao 03

--- front-end/bloco-11-componentes-estado-eventos-formularios-react/dia-1-componentes-estado-eventos/para-fixar/src/FancyButtons.jsx
+++ front-end/bloco-11-componentes-estado-eventos-formularios-react/dia-1-componentes-estado-eventos/para-fixar/src/FancyButtons.jsx
@@ -14,31 +14,45 @@ import React from 'react';
 
 // 4 - Garanta acesso ao objeto this na função que você declarou.
 
+// 5 - Agora você vai converter sua aplicação para uma que conta o número de cliques dado em cada botão!
+// Primeiramente, defina um estado inicial para a contagem de cada botão.
+
+// 6 - Agora, quando um botão for clicado, altere de forma assíncrona o estado deste botão de zero para um.
+
+// 7 - Por fim, baseie-se no estado anterior ao atual para incrementar a contagem de cliques cada vez que um botão for clicado!
+
 class FancyButtons extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      one: 0,
+      two: 0,
+      three: 0,
+    };
+
     this.funcOne = this.funcOne.bind(this);
     this.funcTwo = this.funcTwo.bind(this);
     this.funcThree = this.funcThree.bind(this);
   }
 
-  funcOne = () => console.log('Primeiro botão');
-  funcTwo = () => console.log('Segundo botão');
-  funcThree = () => console.log('Terceiro botão');
+  funcOne = () => this.setState((state, _props) => ({ one: state.one + 1 }));
+  funcTwo = () => this.setState((state, _props) => ({ two: state.two + 1 }));
+  funcThree = () => this.setState((state, _props) => ({ three: state.three + 1 }));
 
   render() {
     return (
       <div className="buttons-container">
         <button className="button-one" onClick={this.funcOne}>
-          Button One
+          A - Cliques: {this.state.one}
         </button>
 
         <button className="button-two" onClick={this.funcTwo}>
-          Button Two
+          B - Cliques: {this.state.two}
         </button>
 
         <button className="button-three" onClick={this.funcThree}>
-          Button Three
+          C - Cliques: {this.state.three}
         </button>
       </div>
     );
