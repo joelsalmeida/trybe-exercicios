0643b4614ca0078dd68fc33db7db4e25aa800695
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-26 15:55:09 -0300

    Test: Verifica se uma piada é renderizada quando a aplicação inicia

--- front-end/bloco-14-testes-automatizados-react-testing-library/dia-2-rtl-mocks-inputs/dad-jokes-app/src/App.test.js
+++ front-end/bloco-14-testes-automatizados-react-testing-library/dia-2-rtl-mocks-inputs/dad-jokes-app/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verifica se uma piada é renderizada quando a aplicação inicia', async () => {
+  global.fetch = (url) => {
+    return Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          id: 'R7UfaahVfFd',
+          joke: 'My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.',
+          status: 200,
+        }),
+    });
+  };
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const joke = await screen.findByText(
+    'My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.'
+  );
+  expect(joke).toBeInTheDocument();
 });
