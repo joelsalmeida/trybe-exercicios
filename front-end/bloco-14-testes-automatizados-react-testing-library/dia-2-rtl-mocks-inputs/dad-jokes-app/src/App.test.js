78323457fa730349f98541b44818eb8a4231d99c
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-26 16:15:33 -0300

    Adiciona clearAllMock, checa se a funçao "fetch" foi chamada uma vez, checa se a funçao "fetch" foi chamada com os parametros corretos

--- front-end/bloco-14-testes-automatizados-react-testing-library/dia-2-rtl-mocks-inputs/dad-jokes-app/src/App.test.js
+++ front-end/bloco-14-testes-automatizados-react-testing-library/dia-2-rtl-mocks-inputs/dad-jokes-app/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+afterEach(() => jest.clearAllMocks());
+
 test('Verifica se uma piada é renderizada quando a aplicação inicia', async () => {
   global.fetch = jest.fn().mockResolvedValue({
     json: jest.fn().mockResolvedValue({
@@ -14,5 +16,10 @@ test('Verifica se uma piada é renderizada quando a aplicação inicia', async (
   const joke = await screen.findByText(
     'My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.'
   );
+
   expect(joke).toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(1);
+  expect(global.fetch).toBeCalledWith('https://icanhazdadjoke.com/', {
+    headers: { Accept: 'application/json' },
+  });
 });
