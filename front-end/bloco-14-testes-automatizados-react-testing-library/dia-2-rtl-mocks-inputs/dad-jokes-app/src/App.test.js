9daa40bbfda8daa725c3e48795468b2aa8ccbde3
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-26 16:00:16 -0300

    Refactoring: Utiliza "jest.fn().mockResolvedValue"

--- front-end/bloco-14-testes-automatizados-react-testing-library/dia-2-rtl-mocks-inputs/dad-jokes-app/src/App.test.js
+++ front-end/bloco-14-testes-automatizados-react-testing-library/dia-2-rtl-mocks-inputs/dad-jokes-app/src/App.test.js
@@ -2,16 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('Verifica se uma piada é renderizada quando a aplicação inicia', async () => {
-  global.fetch = (url) => {
-    return Promise.resolve({
-      json: () =>
-        Promise.resolve({
-          id: 'R7UfaahVfFd',
-          joke: 'My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.',
-          status: 200,
-        }),
-    });
-  };
+  global.fetch = jest.fn().mockResolvedValue({
+    json: jest.fn().mockResolvedValue({
+      id: 'R7UfaahVfFd',
+      joke: 'My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.',
+      status: 200,
+    }),
+  });
 
   render(<App />);
   const joke = await screen.findByText(
