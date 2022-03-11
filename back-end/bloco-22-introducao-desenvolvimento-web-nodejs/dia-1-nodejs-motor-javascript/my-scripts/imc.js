78d1296a229ee8c340d6ee893a5fb0dda613f94f
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-03-11 20:25:04 -0300

    Adiciona categorias de peso de acordo com cada imc

--- back-end/bloco-22-introducao-desenvolvimento-web-nodejs/dia-1-nodejs-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-desenvolvimento-web-nodejs/dia-1-nodejs-motor-javascript/my-scripts/imc.js
@@ -1,8 +1,9 @@
 const readlineSync = require('readline-sync');
 
-// Chegou a hora de tornar nosso script mais interativo! Vamos adicionar input de quem usa.
+// Vamos sofisticar um pouco mais nosso script.
+// Além de imprimir o IMC na tela, imprima também em qual categoria de IMC a pessoa se enquadra.
 
-const NAME = readlineSync.question('Qual o seu nome?');
+const NAME = readlineSync.question('Qual o seu nome? ');
 
 const WEIGHT = readlineSync.questionInt('Qual o seu peso? ', {
   limitMessage: 'Ops, valor inválido. Se seu peso é 75kg, tente 75.',
@@ -14,4 +15,14 @@ const HEIGHT = readlineSync.questionFloat('Qual a sua altura? ', {
 
 const imc = () => (WEIGHT / Math.pow(HEIGHT, 2)).toFixed(2);
 
+const result = (imc) => {
+  if (imc < 18.5) return 'Categoria: Abaixo do peso (magreza).';
+  if (imc > 40.0) return 'Categoria: Obesidade graus III e IV.';
+  if (imc > 35.0) return 'Categoria: Obesidade grau II.';
+  if (imc > 30.0) return 'Categoria: Obesidade grau I.';
+  if (imc > 25.0) return 'Categoria: Acima do peso (sobrepeso).';
+  return 'Categoria: Peso normal.';
+}
+
 console.log(`${NAME}, seu imc é ${imc()}.`);
+console.log(result(imc()));
