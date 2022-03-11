65d96b4fe518bab2ec261119fad6e8d16d56da5b
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-03-11 19:27:58 -0300

    Adiciona o pacote readline-sync. Agora o nome, peso e altura sao adicionados dinamicamente

--- back-end/bloco-22-introducao-desenvolvimento-web-nodejs/dia-1-nodejs-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-desenvolvimento-web-nodejs/dia-1-nodejs-motor-javascript/my-scripts/imc.js
@@ -1,13 +1,17 @@
-// Crie um script para calcular o Índice de Massa Corporal(IMC) de uma pessoa.
-// A fórmula para calcular o IMC é peso / altura ^ 2.
-// Comece criando um novo pacote node com npm init e respondendo às perguntas do npm .
-// Por enquanto, não se preocupe em pedir input da pessoa usuária. Utilize valores fixos para peso e altura .
-// Armazene o script no arquivo imc.js .
+const readlineSync = require('readline-sync');
 
-const NAME = 'Jon';
-const WEIGHT = 75;
-const HEIGHT = 1.80;
+// Chegou a hora de tornar nosso script mais interativo! Vamos adicionar input de quem usa.
+
+const NAME = readlineSync.question('Qual o seu nome?');
+
+const WEIGHT = readlineSync.questionInt('Qual o seu peso? ', {
+  limitMessage: 'Ops, valor inválido. Se seu peso é 75kg, tente 75.',
+});
+
+const HEIGHT = readlineSync.questionFloat('Qual a sua altura? ', {
+  limitMessage: 'Ops, valor inválido. Se sua altura é 1,80m, tente 1.80.',
+});
 
 const imc = () => (WEIGHT / Math.pow(HEIGHT, 2)).toFixed(2);
 
-console.log(imc());
+console.log(`${NAME}, seu imc é ${imc()}.`);
