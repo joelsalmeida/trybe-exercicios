ca6885819c0923d16f3f2d15bbb8e66eb93475dc
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-03-14 09:42:25 -0300

    Exercicios 1 a 4 - selecao basica

---
+++ back-end/bloco-20-introducao-sql/dia-2-encontrando-dados/Exercises-1-4.sql
@@ -0,0 +1,11 @@
+-- Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution". 
+SELECT "This is SQL Exercise, Practice and Solution" AS 'title';
+
+-- Escreva uma query para exibir três números em três colunas.
+SELECT 10 AS 'First_number', 20 AS 'Second_number', 30 AS 'Third_number';
+
+-- Escreva uma query para exibir a soma dos números 10 e 15.
+SELECT (10 + 30) AS 'Soma: 10 + 30';
+
+-- Escreva uma query para exibir o resultado de uma expressão aritmética.
+SELECT (5 * 3) + 2 AS '(5 * 3) + 2';
