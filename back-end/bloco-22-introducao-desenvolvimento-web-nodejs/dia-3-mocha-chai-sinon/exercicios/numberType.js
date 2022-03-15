c7c0c5018be4c5c7aa08a65a5cb3de9f60662113
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-03-15 20:26:50 -0300

    Ajusta funcao numberType caso nao receba parametros

--- back-end/bloco-22-introducao-desenvolvimento-web-nodejs/dia-3-mocha-chai-sinon/exercicios/numberType.js
+++ back-end/bloco-22-introducao-desenvolvimento-web-nodejs/dia-3-mocha-chai-sinon/exercicios/numberType.js
@@ -1,4 +1,7 @@
 const numberType = (value) => {
+  if (value === undefined) throw new Error('Ops, esqueceu o parâmetro?');
+  if (typeof value !== 'number') throw new Error('O valor deve ser um número');
+
   if (value === 0) return 'Neutro';
   if (value > 0) return 'Positivo';
   return 'Negativo';
