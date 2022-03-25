883d4cd1016de3dadea0f8fcee2976f4683d9e87
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-03-25 10:07:14 -0300

    Cria rota GET /books e /books/:id

--- back-end/bloco-23-nodejs-camada-servico-arquitetura-rest-restful/dia-1-arquitetura-software-camada-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-servico-arquitetura-rest-restful/dia-1-arquitetura-software-camada-model/model-example/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const app = express();
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
   return res.status(200).json(authors);
+});
+
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const books = await Book.getByAuthorId(id);
+  return res.status(200).json(books);
+});
+
+app.get('/books', async (_req, res) => {
+  const books = await Book.getAll();
+  return res.status(200).json(books);
 });
 
 const PORT = process.env.PORT || 3000;
