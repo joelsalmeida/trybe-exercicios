bf5fb33677ea57d22f1100836f5fa423785c2184
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-03-28 20:47:26 -0300

    Refactoring - Padroniza rotas, status codes e formato de retorno

--- back-end/bloco-23-nodejs-camada-servico-arquitetura-rest-restful/dia-3-arquitetura-rest-e-restful/product-management/index.js
+++ back-end/bloco-23-nodejs-camada-servico-arquitetura-rest-restful/dia-3-arquitetura-rest-e-restful/product-management/index.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
+const productController = require('./controllers/productController');
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/products', require('./controllers/productController'));
+app.use('/products', productController);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
