ae91a2a06eb8e56dabb96bc7067bd77816182607
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-03-28 20:03:31 -0300

    Inicia base da aplicacao para refactoring

---
+++ back-end/bloco-23-nodejs-camada-servico-arquitetura-rest-restful/dia-3-arquitetura-rest-e-restful/product-management/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/products', require('./controllers/productController'));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log('App listening on port 3000!');
+});
