9cf4d5c414d16b2832b39d62bc16b9c03d3acc13
Joel Almeida <joel.almeida.sa@gmail.com>
2022-05-17 22:22:06 -0300

    cria classe item

---
+++ back-end/bloco-27-poo-solid/dia-1-introducao-orientacao-objetos/src/item.ts
@@ -0,0 +1,18 @@
+export class Item {
+  private _name: string;
+
+  private _price: number;
+
+  constructor(name: string, price: number) {
+    this._name = name;
+    this._price = price;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get price() {
+    return this._price
+  }
+}
